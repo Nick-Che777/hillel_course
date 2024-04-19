ae572844f9e260b7aac7d9460b91a22d73e49796
Nick-Che777 <167120296+Nick-Che777@users.noreply.github.com>
2024-04-19 19:02:45 +0300

    for teacher

--- bonus_lesson/homework2.py
+++ bonus_lesson/homework2.py
@@ -1,1 +1,11 @@
-print("Hello World")
+x=input('4 digit number:')
+x=int(x)
+first_digit=x//1000
+second_digit=x//100%10
+third_digit=x//10%10
+fourth_digit=x%10
+print(first_digit)
+print(second_digit)
+print(third_digit)
+print(fourth_digit)
+
